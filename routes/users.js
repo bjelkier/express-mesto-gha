deff05e8b82a9f5bb1dc0f552948b38eb2429583
Albina Makarova <albinamakarova@rambler.ru>
2023-08-28 05:08:25 +0300

    the result of the 13th sprint with some edits

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const userRouter = require('express').Router();
 const {
-  getUsers, getUserById, createUser, updateUser, updateAvatar,
+  getUsers, getUserById, createUser, updateUser,
 } = require('../controllers/users');
 
 userRouter.get('/', getUsers);
 userRouter.get('/:id', getUserById);
 userRouter.post('/', createUser);
 userRouter.patch('/me', updateUser);
-userRouter.patch('/me/avatar', updateAvatar);
+userRouter.patch('/me/avatar', updateUser);
 
 module.exports = userRouter;
