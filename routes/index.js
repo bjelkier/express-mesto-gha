99fcf398e694ed071d8cb435dfe747d92ed3187d
Albina Makarova <albinamakarova@rambler.ru>
2023-08-28 15:37:54 +0300

    the result of the 13th sprint, 2nd iteration

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
+const NOT_FOUND = 404;
+
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 router.use('*', (req, res) => {
-  res.status(404).send({ message: 'Неверный путь' });
+  res.status(NOT_FOUND).send({ message: 'Неверный путь' });
 });
 
 module.exports = router;
